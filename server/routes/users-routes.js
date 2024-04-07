fad553262fa5b64de425ca338f9ddaf4e781c2f9
MarcoClassUse Xu <shuateng@163.com>
2024-04-07 05:22:11 +0000

    user signup and login function, user input validation

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -1,12 +1,27 @@
 const express = require("express");
-
+const { check } = require("express-validator");
 const usersController = require("../controllers/users-controller");
 
 const router = express.Router();
 
-
-
 router.get("/", usersController.getUsers);
 
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isStrongPassword({
+      minLength: 6,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    }),
+  ],
+  usersController.signup
+);
+
+router.post("/login", usersController.login);
 
 module.exports = router;
