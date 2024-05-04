9f48aafde71b7d31367e8b7dae3e5a524cbb41b9
Levi Zhu <levi970516@gmail.com>
2024-05-04 00:21:02 -0700

    Basic validation and image field for user Avatar

--- client/src/users/components/SignupForm.jsx
+++ client/src/users/components/SignupForm.jsx
@@ -1,7 +1,7 @@
 import { Form } from "react-router-dom";
 import Password from "./Password";
 
-export default function LoginForm() {
+export default function SignupForm() {
   return (
     <Form method="post" className="signup-form" replace>
       <div className="form-row">
@@ -10,6 +10,7 @@ export default function LoginForm() {
           name="fullname"
           type="text"
           placeholder="Full Name"
+          required
         />
       </div>
       <div className="form-row">
@@ -18,11 +19,24 @@ export default function LoginForm() {
           name="email"
           type="email"
           placeholder="Email Address"
+          required
         />
       </div>
 
-      <Password customClass="signup" eyeTheme="white"></Password>
-
+      <Password
+        customClass="signup"
+        eyeTheme="white"
+        patternEnabled="true"
+      ></Password>
+      <div className="form-row">
+        <input
+          id="imageInput"
+          className="signup-input"
+          name="image"
+          type="file"
+          required
+        />
+      </div>
       <div className="form-row">
         <button className="create-account-button">Create Account</button>
       </div>
