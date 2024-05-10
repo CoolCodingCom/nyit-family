ee1ca1a752d5ba0c4bcc85f206aeeb9cdec1b723
MarcoClassUse Xu <shuateng@163.com>
2024-05-10 06:01:39 +0000

    add keys.js

---
+++ server/config/keys.js
@@ -0,0 +1,25 @@
+module.exports = {
+	google: {
+		CLIEND_ID: " ",
+		CLIEND_SECRET: ""
+	},
+	mongoDB: {
+		MONGODB_USER: "",
+		MONGODB_PASSWORD: "",
+		MONGODB_DATABASE: ""
+	},
+	token: {
+		PRIVATE_KEY: ""
+	},
+	mailtrap: {
+		PASSWORD: ""
+	},
+	email: {
+		BACKEND_URL: "http://localhost:5000",
+		SENDER: "",
+		RECEIVER: ""
+	},
+	frontend: {
+		FRONTEND_URL: "http://localhost:5173",
+	}
+};
