6b403c662a61f39558eeb52eaa050003e1716ae3
Levi Zhu <levi970516@gmail.com>
2024-05-14 15:31:07 -0700

    delete sensitive credentials in nodemon.json and correct code format of passportUtil.js

--- server/util/passportUtil.js
+++ server/util/passportUtil.js
@@ -1,68 +1,71 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 const keys = require("../config/keys");
 const cookieSession = require("cookie-session");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const User = require("../models/users");
 
-const passportUtil = app => {
+const passportUtil = (app) => {
   app.use(
     cookieSession({
       keys: ["my_secret"],
-      maxAge: 1000 * 60 * 60 * 24 // 1 day
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
     })
-  )
-  app.use(passport.initialize())
-  app.use(passport.session())
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  passport.use(new GoogleStrategy({
-    clientID: keys.google.CLIEND_ID,
-    clientSecret: keys.google.CLIEND_SECRET,
-    callbackURL: "http://localhost:5000/api/auth/google/callback"
-  },
-  async (accessToken, refreshToken, profile, cb) => {
-    // create new user and store the user into database ot just fetch the user
-    const email = profile._json.email;
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.google.CLIEND_ID,
+        clientSecret: keys.google.CLIEND_SECRET,
+        callbackURL: "http://localhost:5000/api/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        // create new user and store the user into database or just fetch the user
+        const email = profile._json.email;
 
-    let existedUser;
-    try {
-      existedUser = await User.findOne({ email });
-    } catch (error) {
-      throw error;
-    }
-  
-    if (existedUser) {
-      return cb(null, existedUser);
-    }
-  
-    const newUser = new User({
-      name: profile._json.name,
-      email,
-      password: uuidv4(),
-      image: profile._json.picture,
-      isValid: true,
-      uniqueString: uuidv4(),
-      posts: [],
-    });
-  
-    try {
-      await newUser.save();
-    } catch (error) {
-      throw error;
-    }
-  
-    return cb(null, newUser);
-  }
-));
+        let existedUser;
+        try {
+          existedUser = await User.findOne({ email });
+        } catch (error) {
+          throw error;
+        }
+
+        if (existedUser) {
+          return cb(null, existedUser);
+        }
+
+        const newUser = new User({
+          name: profile._json.name,
+          email,
+          password: uuidv4(),
+          image: profile._json.picture,
+          isValid: true,
+          uniqueString: uuidv4(),
+          posts: [],
+        });
+
+        try {
+          await newUser.save();
+        } catch (error) {
+          throw error;
+        }
+
+        return cb(null, newUser);
+      }
+    )
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user)
-  })
+    done(null, user);
+  });
 
   passport.deserializeUser((user, done) => {
-    done(null, user)
-  })
-}
+    done(null, user);
+  });
+};
 
-module.exports = passportUtil
+module.exports = passportUtil;
