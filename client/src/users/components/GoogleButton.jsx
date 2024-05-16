c915f1ad18196bc3494eec25827409daec8be085
Levi Zhu <levi970516@gmail.com>
2024-05-16 14:49:21 -0700

    refactor signup page to handle signup email verification UI page for user, optimize create account button, move google handler button function into Googlebutton component

--- client/src/users/components/GoogleButton.jsx
+++ client/src/users/components/GoogleButton.jsx
@@ -1,9 +1,21 @@
 import googleIcon from "../../assets/google.svg";
 
-export default function GoogleButton({ customClass, children, onClick }) {
+export default function GoogleButton({ customClass, children }) {
+  const loginWithGoogleHandler = async () => {
+    try {
+      window.location.href = `http://localhost:5000/api/auth/google`;
+    } catch (err) {
+      return err.message;
+    }
+  };
+
   return (
     <>
-      <button className={`${customClass} button-with-image`} type="button" onClick={onClick}>
+      <button
+        className={`${customClass} button-with-image`}
+        type="button"
+        onClick={loginWithGoogleHandler}
+      >
         <img className="google-login-img" src={googleIcon} alt="google" />
         {children}
       </button>
