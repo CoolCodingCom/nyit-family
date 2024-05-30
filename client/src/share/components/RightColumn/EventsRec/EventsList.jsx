9f484bf53c6a3b7b995b03b8d4aea9da5266fb6c
MarcoClassUse Xu <shuateng@163.com>
2024-05-30 00:25:26 +0000

    eventsbox frame

---
+++ client/src/share/components/RightColumn/EventsRec/EventsList.jsx
@@ -0,0 +1,53 @@
+import Event1Img from "./svg/event1.svg";
+import Event2Img from "./svg/event2.svg";
+import Event3Img from "./svg/event3.svg";
+
+import EventItem from "./EventItem";
+
+import "./EventsList.css";
+
+const DUMMY_EVENTS = [
+  {
+    id: "1",
+    theme: "COVID19",
+    timestamp: "last night",
+    content:
+      "England's Chief Medical Officer says the UK is at the most dangerous time of the pandemic",
+    tag: "covid19",
+    NumOfPosts: "100k",
+    img: { Event1Img },
+  },
+  {
+    id: "2",
+    theme: "US news",
+    timestamp: "4h ago",
+    content:
+      "Parler may go offline following suspensions by Amazon, Apple and Google",
+    tag: "trump",
+    NumOfPosts: "100k",
+    img: { Event2Img },
+  },
+  {
+    id: "3",
+    theme: "India",
+    timestamp: "1h ago",
+    content:
+      "India vs Australia: India hold on to earn a draw on Day 5 in Sydney Test",
+    tag: "sport",
+    NumOfPosts: "10k",
+    img: { Event3Img },
+  },
+];
+
+const EventsList = (props) => {
+  return (
+    <div className="">
+      <h3>123</h3>
+      {DUMMY_EVENTS.map((eitem) => (
+        <EventItem key={eitem.id} content={eitem.content} />
+      ))}
+    </div>
+  );
+};
+
+export default EventsList;
