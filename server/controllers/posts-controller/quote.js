5f08e55872a24d6fd61c8d43ff4c00efad3feaf1
Levi Zhu <levi970516@gmail.com>
2024-06-07 15:54:24 -0700

    change author to userId, add one more field username to Post

--- server/controllers/posts-controller/quote.js
+++ server/controllers/posts-controller/quote.js
@@ -2,24 +2,24 @@ const Post = require("../../models/posts.js");
 
 const createQuote = async (req, res) => {
   try {
-    const { author, content, originalPost } = req.body;
+    const { userId, content, originalPost } = req.body;
 
     const post = await Post.findById(originalPost);
     if (!post) {
       return res.status(404).json({ message: "Post not found" });
     }
     const repostIndex = post.reposts.findIndex((repost) =>
-      repost.userId.equals(author)
+      repost.userId.equals(userId)
     );
     if (repostIndex !== -1) {
       // Increment the count if userId is already in reposts
       post.reposts[repostIndex].count += 1;
     } else {
       // Add new repost object if userId is not in reposts
-      post.reposts.push({ userId: author, count: 1 });
+      post.reposts.push({ userId: userId, count: 1 });
     }
     await post.save();
-    await Post.create({ author, content, isQuote: true, originalPost });
+    await Post.create({ userId, content, isQuote: true, originalPost });
     res.status(200).json({ message: "Quote created successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
