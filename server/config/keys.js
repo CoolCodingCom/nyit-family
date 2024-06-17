c2904a5de902ab1f09ccc60099f709a6466f471c
MarcoClassUse Xu <shuateng@163.com>
2024-06-17 06:41:37 +0000

    change back to the original env param

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,27 +1,26 @@
 module.exports = {
-	google: {
-	  CLIEND_ID:
-		"966004257988-njq8ipuib0dgst6ia2lvi9ehpugd4ks9.apps.googleusercontent.com",
-	  CLIEND_SECRET: "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",
-	},
-	mongoDB: {
-	  MONGODB_USER: "leconyit",
-	  MONGODB_PASSWORD: "oxYmaTeZElwr6La8",
-	  MONGODB_DATABASE: "nyit-family-dev",
-	},
-	token: {
-	  PRIVATE_KEY: "leco_most_secure_secret",
-	},
-	mailtrap: {
-	  PASSWORD: "edbb47d4962e414565c07cb365f2ce38",
-	},
-	email: {
-	  BACKEND_URL: "http://localhost:5000",
-	  SENDER: "verification@nyitfamily.site",
-	  RECEIVER: "txu4031@gmail.com",
-	},
-	frontend: {
-	//   FRONTEND_URL: "http://localhost:5173",
-		FRONTEND_URL: "https://5173-coolcodingco-nyitfamily-ecyp8ty0xgk.ws-us114.gitpod.io"
-	},
-  };
+  google: {
+    CLIEND_ID:
+      "966004257988-njq8ipuib0dgst6ia2lvi9ehpugd4ks9.apps.googleusercontent.com",
+    CLIEND_SECRET: "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",
+  },
+  mongoDB: {
+    MONGODB_USER: "leconyit",
+    MONGODB_PASSWORD: "oxYmaTeZElwr6La8",
+    MONGODB_DATABASE: "nyit-family-dev",
+  },
+  token: {
+    PRIVATE_KEY: "leco_most_secure_secret",
+  },
+  mailtrap: {
+    PASSWORD: "edbb47d4962e414565c07cb365f2ce38",
+  },
+  email: {
+    BACKEND_URL: "http://localhost:5000",
+    SENDER: "verification@nyitfamily.site",
+    RECEIVER: "txu4031@gmail.com",
+  },
+  frontend: {
+    FRONTEND_URL: "http://localhost:5173",
+  },
+};
