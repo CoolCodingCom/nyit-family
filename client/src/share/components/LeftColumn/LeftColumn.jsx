05e2eabd4383fb5bf4c58366cd0332fccbd974d3
MarcoXu <157637426+MarcoClassUse@users.noreply.github.com>
2024-06-21 01:07:41 +0000

    just kindly apply the responsive navigation bar

--- client/src/share/components/LeftColumn/LeftColumn.jsx
+++ client/src/share/components/LeftColumn/LeftColumn.jsx
@@ -1,11 +1,37 @@
-import MainNavigation from "./Navigation/MainNavigation";
+import { useState, useEffect } from "react";
+import MainNavigationLong from "./Navigation/MainNavigationLong";
+import MainNavigationIcon from "./Navigation/MainNavigationIcon";
 
 import "./LeftColumn.css";
 
+
 const LeftColumn = (props) => {
+  const [showLongSideBar, setShowLongSideBar] = useState(true);
+  const [showSimSideBar, setShowSimSideBar] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1345) {
+        setShowLongSideBar(false);
+        setShowSimSideBar(true);
+      } else {
+        setShowLongSideBar(true);
+        setShowSimSideBar(false);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize); 
+
+    return () => {
+      window.removeEventListener('resize', handleResize); 
+    };
+  }, []);
+
   return (
     <div className="leftcol__container">
-      <MainNavigation />
+      {showLongSideBar && <MainNavigationLong />}
+      {showSimSideBar && <MainNavigationIcon/>}
     </div>
   );
 };
---
+++ client/src/share/components/LeftColumn/Navigation/MainNavigationIcon.jsx
@@ -0,0 +1,34 @@
+// import { ReactComponent as Logo } from "./svg/logo.svg";
+import NavigationLink from "./NavigationLink";
+
+import "./MainNavigationIcon.css";
+
+const MainNavigationIcon = (props) => {
+  return (
+    <div className="navigation__container">
+      <nav>
+        <NavigationLink url="/" handle="logo" />
+        <NavigationLink url="/" handle="home" />
+        <NavigationLink url="/login" handle="explore" />
+        <NavigationLink
+          url="/login"
+          handle="notifications"
+        />
+        <NavigationLink url="/login" handle="messages" />
+        <NavigationLink url="/login" handle="bookmarks" />
+        <NavigationLink url="/login" handle="profile" />
+        <NavigationLink
+          url="/login"
+          handle="settingandprivacy"
+        />
+      </nav>
+      <div className="navipost__btn">
+        <button type="submit">
+          Post
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MainNavigationIcon;
