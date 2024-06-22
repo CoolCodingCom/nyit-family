f53ca2690ad7a6bd2de6ff9b82ffd533d7f8ea0a
MarcoXu <157637426+MarcoClassUse@users.noreply.github.com>
2024-06-22 08:10:38 +0000

    move navigation bar to LayOut component

--- client/src/home/components/Layout.jsx
+++ client/src/home/components/Layout.jsx
@@ -1,5 +1,15 @@
 import { Outlet } from "react-router-dom";
+import LeftColumn from "../../share/components/LeftColumn/LeftColumn";
+
+import "./Layout.css";
 
 export default function Layout() {
-  return <Outlet />;
+  return (
+    <div className="main-container">
+      {/* <div className="leftCol-wrapper"> */}
+        <LeftColumn />
+      {/* </div> */}
+      <Outlet/>
+    </div>
+  );
 }
