e4ee215c3f6b2cb1ed9c65b6bf38a98d01d82c98
MarcoXu <157637426+MarcoClassUse@users.noreply.github.com>
2024-06-22 08:10:38 +0000

    move navigation bar to LayOut component

--- client/src/home/pages/Home.jsx
+++ client/src/home/pages/Home.jsx
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
 import LeftColumn from "../../share/components/LeftColumn/LeftColumn";
 import MiddleColumn from "../../share/components/MiddleColumn/MiddleColumn";
 import RightColumn from "../../share/components/RightColumn/RightColumn";
 
 import "./Home.css";
 
+
 export default function Home() {
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log('User scrolled:', window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); 
+
   return (
-    <div className="home-container">
-      <LeftColumn />
+    <>
       <MiddleColumn />
       <RightColumn />
-    </div>
+    </>
   );
 }
