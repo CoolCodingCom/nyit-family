56a45b780b5313af1715ad906b446412316fa675
MarcoXu <157637426+MarcoClassUse@users.noreply.github.com>
2024-06-23 06:50:42 +0000

    finish the rightsidebar scrolling flexibility

--- client/src/share/components/RightColumn/RightColumn.jsx
+++ client/src/share/components/RightColumn/RightColumn.jsx
@@ -1,18 +1,32 @@
+import { forwardRef } from "react";
+
 import EventsBox from "./EventsRec/EventsBox";
 import SearchBox from "./Search/SearchBox";
 import PeopleBox from "./PeopleRec/PeopleBox";
 
 import "./RightColumn.css";
 
-
-const RightColumn = (props) => {
+const RightColumn = forwardRef((props, ref) => {
+  const style = props.isFix ? { top: `-${props.offset}px` } : { top: "0" };
   return (
-    <div className="rightcol__container">
-      <SearchBox/>
-      <EventsBox/>
-      <PeopleBox/>
+    <div className="rightcol__wrapper">
+      <div
+        className="basement"
+        style={{ height: `${props.baseHeight}px` }}
+      ></div>
+      <div
+        className={`rightcol__container ${
+          props.isFix && "rightcol__container-bottomfix"
+        }`}
+        style={style}
+        ref={ref}
+      >
+        <SearchBox />
+        <EventsBox />
+        <PeopleBox />
+      </div>
     </div>
   );
-};
+});
 
 export default RightColumn;
