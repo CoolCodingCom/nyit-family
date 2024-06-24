5b8c7eee8dadd7cbc5c67e8a7aaa42babf0f6b00
MarcoXu <157637426+MarcoClassUse@users.noreply.github.com>
2024-06-24 19:04:04 +0000

    add multiple style and text limit indicator

--- client/src/share/components/Elements/Pie.jsx
+++ client/src/share/components/Elements/Pie.jsx
@@ -8,13 +8,12 @@ const cleanPercentage = (percentage) => {
   return tooLow ? 0 : tooHigh ? 100 : +percentage;
 };
 
-const Circle = ({ colour, pct }) => {
-  const r = 10;
-  const circ = 2 * Math.PI * r;
+const Circle = ({ radius, colour, pct }) => {
+  const circ = 2 * Math.PI * radius;
   const strokePct = ((100 - pct) * circ) / 100;
   return (
     <circle
-      r={r}
+      r={radius}
       cx={183}
       cy={15}
       fill="transparent"
@@ -27,15 +26,32 @@ const Circle = ({ colour, pct }) => {
   );
 };
 
-const Pie = ({ percentage, colour }) => {
+const Text = ({ leftChar }) => {
+  return (
+    <text
+      x="45%"
+      y="50%"
+      dominantBaseline="central"
+      textAnchor="middle"
+      fontSize={"0.9em"}
+    >
+      {leftChar.toFixed(0)}
+    </text>
+  );
+};
+
+const Pie = ({ radius, percentage, colour, leftChar }) => {
   const pct = cleanPercentage(percentage);
   return (
     <div className="pie__container">
       <svg width={35} height={35}>
-        <g transform={`rotate(-90 ${"100 100"})`}>
-          <Circle colour="lightgrey" />
-          <Circle colour={colour} pct={pct} />
-        </g>
+        {leftChar > -10 && (
+          <g transform={`rotate(-90 ${"100 100"})`}>
+            <Circle radius={radius} colour="lightgrey" />
+            <Circle radius={radius} colour={colour} pct={pct} />
+          </g>
+        )}
+        {leftChar <= 20 && <Text leftChar={leftChar} />}
       </svg>
     </div>
   );
