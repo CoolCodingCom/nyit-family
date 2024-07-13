9a037c73879dbbfd2103e6f43e6e57840cca9287
MarcoClassUse Xu <shuateng@163.com>
2024-07-13 23:45:28 +0000

    finish the function of people search and user friendly adjustments

--- client/src/share/components/RightColumn/Search/SearchBar.jsx
+++ client/src/share/components/RightColumn/Search/SearchBar.jsx
@@ -5,8 +5,10 @@ import SearchIcon from "./svg/search.svg";
 import "./SearchBar.css";
 import PopList from "../../Elements/PopList";
 import { getUsersByQueryKey } from "../../../../apis/user";
+import OutsideClickHandler from "../../Elements/OutsideClickHandler";
 
 const SearchBar = (props) => {
+  const [showSearchList, setShowSearchList] = useState(false);
   const [searchInput, setSearchInput] = useState("");
   const [userList, setUserList] = useState();
 
@@ -34,9 +36,17 @@ const SearchBar = (props) => {
     setSearchInput("");
   };
 
+  const onInputFocus = () => {
+    setShowSearchList(true);
+  };
+
+  const onOutsideClickHandler = () => {
+    setShowSearchList(false);
+  };
+
   return (
-    <>
-      <div className="search__wrap">
+    <div className="search__wrap">
+      <OutsideClickHandler onOutsideClick={onOutsideClickHandler}>
         <div className="search__inner">
           <img src={SearchIcon} />
           <input
@@ -44,9 +54,10 @@ const SearchBar = (props) => {
             type="text"
             value={searchInput}
             onChange={handleChange}
+            onFocus={onInputFocus}
             placeholder="Search"
           />
-          {searchInput.length > 0 && (
+          {searchInput.length > 0 && showSearchList && (
             <button
               type="button"
               className="search__cancel"
@@ -56,20 +67,18 @@ const SearchBar = (props) => {
             </button>
           )}
         </div>
-      </div>
-      {searchInput.length > 0 && userList && (
-        <PopList
-          show
-          isprofile
-          nobackdrop
-          list={userList}
-          posTop={51.5}
-          width={310}
-          className="morelist__container"
-          // onClick={props.onClick}
-        />
-      )}
-    </>
+        {searchInput.length > 0 && userList && (
+          <PopList
+            show={showSearchList}
+            isprofile
+            nobackdrop
+            list={userList}
+            posTop={51.5}
+            width={310}
+          />
+        )}
+      </OutsideClickHandler>
+    </div>
   );
 };
 
