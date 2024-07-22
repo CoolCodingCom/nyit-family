e3611089ae9671e725240b4d6709ef2e213dd841
Levi Zhu <142374248+Levi-Zhu@users.noreply.github.com>
2024-07-22 15:37:34 -0700

    Update keys.js

---
+++ server/config/keys.js
@@ -0,0 +1,26 @@
+module.exports = {
+  google: {
+    CLIEND_ID:
+      "",
+    CLIEND_SECRET: "",
+  },
+  mongoDB: {
+    MONGODB_USER: "",
+    MONGODB_PASSWORD: "",
+    MONGODB_DATABASE: "",
+  },
+  token: {
+    PRIVATE_KEY: "",
+  },
+  mailtrap: {
+    PASSWORD: "",
+  },
+  email: {
+    BACKEND_URL: "",
+    SENDER: "",
+    RECEIVER: "",
+  },
+  frontend: {
+    FRONTEND_URL: "",
+  },
+};
